79a5859e1013487e5ef24bb111006c9b60ee94dc
PedregosaCH <danielpedchacon@gmail.com>
2026-04-20 12:53:28 +0200

    "Conectado el programa con la base de datos"

--- TaskMasterProgramacion/src/main/java/dam/code/config/DatabaseConfig.java
+++ TaskMasterProgramacion/src/main/java/dam/code/config/DatabaseConfig.java
@@ -1,4 +1,15 @@
 package dam.code.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConfig {
+    private static final String URL = "jdbc:postgresql://localhost:5432/Task_Master";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
